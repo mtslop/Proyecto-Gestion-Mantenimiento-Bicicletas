afdae60231e1b6b733a3022152439dfabf85d077
Rsor19 <royergs.orozcoramirez@uqvirtual.edu.co>
2026-09-13 00:36:04 -0500

    Archivo module implementado a la aplicación

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,7 @@
 module Proyecto.Gestion.Mantenimiento.Bicicletas {
+    requires javafx.controls;
+    requires javafx.fxml;
+    exports co.uniquindio.edu.poo;
+    exports co.uniquindio.edu.poo.model;
+    opens co.uniquindio.edu.poo.controller to javafx.fxml;
 }
